8d5614002a4b7df3a962125ed01b38915d4af87d
DCoates <DCoates@Rubiconsoftware.com>
2019-07-24 09:13:02 +0100

    Start Deeper Look at Labels

---
+++ Ch03_DeeperIntoText/Ch03_DeeperIntoText/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ch03_DeeperIntoText
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Label1.Text = $"Screen siren Gina Lollobrigida, 92, was 'conned out of 3m euros, houses, and jewellery' by ex-chauffeur-turned-manager, 32, who 'raided her fortune to buy Ferraris and motorcycles after he gained control of the ''narcissistic'' former actress's estate' ";
+            Label1.HorizontalOptions = LayoutOptions.Start;
+            Label1.HorizontalTextAlignment = TextAlignment.Center;
+            Label1.VerticalOptions = LayoutOptions.Start;
+            Label1.VerticalTextAlignment = TextAlignment.End;   //NonSerializedAttribute Effect!
+            Padding = new Thickness(10, Device.OnPlatform(20, 10, 50), 10, 5);
+
+        }
+    }
+}
