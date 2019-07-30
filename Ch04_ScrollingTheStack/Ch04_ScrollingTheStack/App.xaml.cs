b6b3dc6b9b4f38ae2beeac47e0a9bd1ddf620bc8
DCoates <DCoates@Rubiconsoftware.com>
2019-07-30 08:59:24 +0100

    Start Colour Reflection Page

---
+++ Ch04_ScrollingTheStack/Ch04_ScrollingTheStack/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Ch04_ScrollingTheStack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage
+            {
+                Padding = new Thickness(10, TTop(), right: 10, bottom: 5)
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        private double TTop()
+        {
+            double top = 0.0d;
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    top = 20d;
+                    break;
+                case Device.Android:
+                    top = 10d;
+                    break;
+                case Device.UWP:
+                    top = 40d;
+                    break;
+            }
+
+            return top;
+        }
+    }
+}
---
+++ Ch04_ScrollingTheStack/Ch04_ScrollingTheStack/ReflectedColoursPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+using Xamarin.Forms;
+
+namespace Ch04_ScrollingTheStack
+{
+	public class ReflectedColoursPage : ContentPage
+	{
+		public ReflectedColoursPage ()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Welcome to the Reflected Colours Page", HorizontalOptions = LayoutOptions.Center }
+				}
+			};
+
+            foreach (var info in typeof(Color).GetRuntimeFields())
+            {
+
+            }
+		}
+	}
+}
