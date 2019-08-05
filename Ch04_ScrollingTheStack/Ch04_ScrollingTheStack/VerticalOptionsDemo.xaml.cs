1a20a447481aee65749c6c04e01f4908e5c1b901
DCoates <DCoates@Rubiconsoftware.com>
2019-08-05 09:20:39 +0100

    Vertical Stuff so you can see what each Veritcal Alignment does.

--- Ch04_ScrollingTheStack/Ch04_ScrollingTheStack/VerticalOptionsDemo.xaml.cs
+++ Ch04_ScrollingTheStack/Ch04_ScrollingTheStack/VerticalOptionsDemo.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Ch04_ScrollingTheStack
@@ -9,6 +12,35 @@ namespace Ch04_ScrollingTheStack
 		public VerticalOptionsDemo ()
 		{
 			InitializeComponent ();
-		}
+
+            Color[] colors = {Color.Yellow, Color.Green};
+            int yGIdx = 0;
+
+            IEnumerable<Label> labels = from field in typeof(LayoutOptions).GetRuntimeFields()
+                where field.IsPublic && field.IsStatic
+                orderby ((LayoutOptions) field.GetValue(null)).Alignment
+                select new Label
+                {
+                    Text = $"VerticalOptions = {field.Name}",
+                    VerticalOptions = (LayoutOptions) field.GetValue(null),
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                    TextColor = colors[yGIdx],
+                    BackgroundColor = colors[yGIdx = 1 - yGIdx]
+                };
+
+            StackLayout sLayout = new StackLayout();
+
+            //foreach (var l in labels.Select((value, i) => new { i, value }).OrderByDescending(v => v.i))
+            foreach (var l in labels.Select((value, i) => new {i, value}))
+            {
+                //if (l.i < 2) continue;
+                l.value.Text = $"{l.i}  {l.value.Text}";
+                sLayout.Children.Add(l.value);
+                //if (l.i >= 2) break;
+            }
+
+            Content = sLayout;
+        }
 	}
 }
