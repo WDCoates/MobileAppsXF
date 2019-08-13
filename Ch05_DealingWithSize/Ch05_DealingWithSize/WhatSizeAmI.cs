9b8451914b03c024a02472ec035c8689f52e2ecd
DCoates <DCoates@Rubiconsoftware.com>
2019-08-13 09:10:34 +0100

    Working with Size and Fixing Andriod Orientation

---
+++ Ch05_DealingWithSize/Ch05_DealingWithSize/WhatSizeAmI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Ch05_DealingWithSize
+{
+	public class WhatSizeAmI : ContentPage
+    {
+        private readonly Label WidthXHight;
+        
+		public WhatSizeAmI ()
+        {
+
+            WidthXHight = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            WidthXHight = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+
+
+            Content = WidthXHight;
+
+            SizeChanged += OnPageSizeChanged;
+		}
+
+        void OnPageSizeChanged(object sender, EventArgs args)
+        {
+            WidthXHight.Text = $"{Width} \u00D7 {Height}";
+        }
+	}
+}
