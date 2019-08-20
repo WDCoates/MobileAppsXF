a444a812c12d9acae567ec97b49cc037a359f77c
DCoates <DCoates@Rubiconsoftware.com>
2019-08-20 08:59:39 +0100

    Estimating the fontsize required form the size of a view when changed.

--- Ch05_DealingWithSize/Ch05_DealingWithSize/FontSizesEstimated.cs
+++ Ch05_DealingWithSize/Ch05_DealingWithSize/FontSizesEstimated.cs
@@ -20,7 +20,44 @@ namespace Ch05_DealingWithSize
                 Content = _label
             };
 
+            cView.SizeChanged += OnContentViewSizeChanged;
+
+            Content = cView;
+
         }
+
+        void OnContentViewSizeChanged(object sender, EventArgs eArgs)
+        {
+
+            // Get the 'View' that is changing
+            View view = (View)sender;
+
+            var lineHeight = LineHeight();
+            var charWidth = 0.5;
+
+            var text = $"A system font created by specifying a size of S has a line height of about ({lineHeight:F1} * S) and " +
+                            $"an average character width of about ({charWidth:F1} * S). " +
+                            $"On this page, which has a width of {view.Width:F0} and " +
+                            $"a height of {view.Height:F0}, a font size of ?1 should comfortably render the ??2 characters in this " +
+                            "paragraph with ?3 lines and about ?4 characters per line. Does it work?";
+
+            var charCount = text.Length;
+            var fontSize = (int) Math.Sqrt(view.Width * view.Height / (charCount * lineHeight * charWidth));
+
+            var lineCount = (int) (view.Height / (lineHeight * fontSize));
+            var charsPerLine = (int) (view.Height / (charWidth * fontSize));
+
+            text = text.Replace("?1", fontSize.ToString());
+            text = text.Replace("??2", charCount.ToString());
+            text = text.Replace("?3", lineCount.ToString());
+            text = text.Replace("?4", charsPerLine.ToString());
+
+            _label.Text = text;
+            _label.FontSize = fontSize;
+
+        }
+
+
 
         double TTop()
         {
@@ -39,6 +76,18 @@ namespace Ch05_DealingWithSize
             }
 
             return top;
+        }
+
+        double LineHeight()
+        {
+            double lineHeight = 1.2d;
+            switch (Device.RuntimePlatform)
+            {
+                case Device.UWP:
+                    lineHeight = 1.3d;
+                    break;
+            }
+            return lineHeight;
         }
     }
 
