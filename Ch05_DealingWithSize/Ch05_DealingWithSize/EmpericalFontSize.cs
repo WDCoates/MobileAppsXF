616c2e08786ed376195afdccc7cf71619e6e310f
DCoates <DCoates@Rubiconsoftware.com>
2019-08-30 17:31:27 +0100

    Almost finished...

--- Ch05_DealingWithSize/Ch05_DealingWithSize/EmpericalFontSize.cs
+++ Ch05_DealingWithSize/Ch05_DealingWithSize/EmpericalFontSize.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using Xamarin.Forms;
 
@@ -16,14 +13,38 @@ namespace Ch05_DealingWithSize
             label = new Label();
 
             Padding = new Thickness(0, Helper.TTop());
-            
-            
+            ContentView cView = new ContentView
+            {
+                Content = label
+            };
+
+            cView.SizeChanged += OnContentViewSizeChanged;
             
             Content = new StackLayout
             {
                 Children = {
+                    cView
                 }
             };
+        }
+
+        private void OnContentViewSizeChanged(object sender, EventArgs e)
+        {
+            View view = (View)sender;
+
+            if (view.Width <= 0 || view.Height <= 0)
+                return;
+
+            label.Text = $"This is just some text to try and illustrate how best to fit text to a screen....";
+
+            FontCalc lFontCalc = new FontCalc(label, 10, view.Width);
+            FontCalc uFontCalc = new FontCalc(label, 100, view.Width);
+
+            while (uFontCalc.FontSize - lFontCalc.FontSize > 1)
+            {
+                // Get the average font size
+            }
+
         }
     }
 }
---
+++ Ch05_DealingWithSize/Ch05_DealingWithSize/FontCalc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Ch05_DealingWithSize
+{
+    public struct FontCalc
+    {
+        public double FontSize { private set; get;}
+        public double TextHeight { private set; get; }
+        public FontCalc(Label label, double fSize, double cWidth) : this()
+        {
+            //Save the font size for ref.
+            FontSize = fSize;
+
+            //Recalculate the Label height.
+            label.FontSize = fSize;
+            SizeRequest sizeReq = label.GetSizeRequest(cWidth, Double.PositiveInfinity);
+
+            //Save this height
+            TextHeight = sizeReq.Request.Height;
+        }
+
+    }
+}
