d8c62b0847863fbcfd07a4b92d76dd8878c434e7
DCoates <DCoates@Rubiconsoftware.com>
2019-09-04 09:08:00 +0100

    Simple Button Click Logger

---
+++ Ch06_ButtonClicks/Ch06_ButtonClicks/ButtonLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Ch06_ButtonClicks
+{
+	public class ButtonLogger : ContentPage
+    {
+        private StackLayout loggerLayout = new StackLayout();
+
+		public ButtonLogger ()
+		{
+            //Create the button and attach handler
+            Button button = new Button
+            {
+                Text = $"Log the Click and Time"
+            };
+            button.Clicked += OnButtonClicked;
+
+            this.Padding = new Thickness(5, 10);
+
+			Content = new StackLayout {
+				Children = {
+                    new Label { Text = "Welcome to Button Logger in Xamarin.Forms!" },
+                    button,
+                    new ScrollView
+                    {
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        Content = loggerLayout
+                    }
+					
+				}
+			};
+		}
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            loggerLayout.Children.Add(new Label
+            {
+                Text = $"Button clicked at {DateTime.Now:T}"
+            });
+        }
+    }
+}
