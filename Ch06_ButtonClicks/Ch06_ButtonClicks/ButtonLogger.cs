4d0cfec8a553ee89f9972372a0d61de700522091
DCoates <DCoates@Rubiconsoftware.com>
2019-09-05 08:23:25 +0100

    Added Event Handler for when Children as added to stacklayouts.

--- Ch06_ButtonClicks/Ch06_ButtonClicks/ButtonLogger.cs
+++ Ch06_ButtonClicks/Ch06_ButtonClicks/ButtonLogger.cs
@@ -8,18 +8,20 @@ namespace Ch06_ButtonClicks
         private StackLayout loggerLayout = new StackLayout();
 
 		public ButtonLogger ()
-		{
+        {
             //Create the button and attach handler
             Button button = new Button
             {
                 Text = $"Log the Click and Time"
             };
             button.Clicked += OnButtonClicked;
+            loggerLayout.ChildAdded += GetOnChildAdded;
 
             this.Padding = new Thickness(5, 10);
 
-			Content = new StackLayout {
-				Children = {
+            Content = new StackLayout
+            {
+                Children = {
                     new Label { Text = "Welcome to Button Logger in Xamarin.Forms!" },
                     button,
                     new ScrollView
@@ -27,10 +29,17 @@ namespace Ch06_ButtonClicks
                         VerticalOptions = LayoutOptions.FillAndExpand,
                         Content = loggerLayout
                     }
-					
-				}
-			};
-		}
+
+                }
+            };
+        }
+
+        private void GetOnChildAdded(object sender, ElementEventArgs e)
+        {
+            var StackL = (Layout)sender;
+            ((Label) ((Layout) sender).Children[StackL.Children.Count-1]).Text += $"!";
+
+        }
 
         private void OnButtonClicked(object sender, EventArgs e)
         {
