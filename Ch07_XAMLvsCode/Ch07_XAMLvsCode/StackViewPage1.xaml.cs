da15479393caa2680a1e242c9ebbf8c3ec42b8a2
DCoates <DCoates@Rubiconsoftware.com>
2019-09-27 08:40:20 +0100

    Adding Children to the Content StackLayout after XAML created StackLayout

--- Ch07_XAMLvsCode/Ch07_XAMLvsCode/StackViewPage1.xaml.cs
+++ Ch07_XAMLvsCode/Ch07_XAMLvsCode/StackViewPage1.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Ch07_XAMLvsCode
@@ -15,6 +9,37 @@ namespace Ch07_XAMLvsCode
 		public StackViewPage1 ()
 		{
 			InitializeComponent ();
-		}
+
+            Label label = new Label
+            {
+                Text = "Label added in code!",
+                IsVisible = true,
+                Opacity = 0.75,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.Blue,
+                BackgroundColor = Color.FromRgb(255, 128, 128),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+            };
+            var c = ((StackLayout)Content).Children.Count;
+            ((StackLayout)Content).Children.Insert(0, label);
+            
+            Label label2 = new Label
+            {
+                Text = "Label added in code! No easy way to copy an object in c#!",
+                IsVisible = true,
+                Opacity = 0.75,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.Blue,
+                BackgroundColor = Color.FromRgb(255, 128, 128),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+            };
+            
+
+            ((StackLayout) Content).Children.Insert(c+1, label2);
+        }
 	}
 }
