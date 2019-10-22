65ba4e612178b990cc9a0c546b85413ca6a3ded8
DCoates <DCoates@Rubiconsoftware.com>
2019-10-22 08:56:42 +0100

    Platform Specifics using Standard Projects and DependencyService.Get requires Interface def and assembly decoration.

---
+++ Ch09_PltfrmInfDpndSrvc/Ch09_PltfrmInfDpndSrvc.UWP/PlatformSpecificInfo.cs
@@ -0,0 +1,23 @@
+﻿using Windows.Security.ExchangeActiveSyncProvisioning;
+using Ch09_PltfrmInfDpndSrvc;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Ch09_PlatformSpecificAPICalls.PlatformSpecificInfo))]
+
+namespace Ch09_PlatformSpecificAPICalls
+{
+    class PlatformSpecificInfo : IPlatformInfo
+    {
+        internal EasClientDeviceInformation _devInfo = new EasClientDeviceInformation();
+
+        public string GetModel()
+        {
+            return $"{_devInfo.SystemManufacturer}, {_devInfo.SystemProductName}";
+        }
+
+        public string GetVersion()
+        {
+            return _devInfo.OperatingSystem;
+        }
+    }
+}
---
+++ Ch09_PltfrmInfDpndSrvc/Ch09_PltfrmInfDpndSrvc/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ch09_PltfrmInfDpndSrvc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            IPlatformInfo platformInfo = DependencyService.Get<IPlatformInfo>();
+            lDeviceModel.Text = platformInfo.GetModel();
+            lVersion.Text = platformInfo.GetVersion();
+        }
+    }
+}
