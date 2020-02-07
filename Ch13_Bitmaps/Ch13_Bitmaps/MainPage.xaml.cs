ee67a1efdb5ee8a2ebe3ad2907985ba419b0b35b
DCoates <DCoates@Rubiconsoftware.com>
2020-02-07 09:20:57 +0000

    Start the Mad Park Page.

--- Ch13_Bitmaps/Ch13_Bitmaps/MainPage.xaml.cs
+++ Ch13_Bitmaps/Ch13_Bitmaps/MainPage.xaml.cs
@@ -28,7 +28,9 @@ namespace Ch13_Bitmaps
 
             //S5_DeviceIndBitmapSize()
             Button5.Clicked += (sender, args) => { Navigation.PushAsync(new S5_DeviceIndBitmapSize()); };
-            
+
+            //S5_DeviceIndBitmapSize()
+            Button6.Clicked += (sender, args) => { Navigation.PushAsync(new S6_MadStJamesPark()); };
         }
     }
 }
---
+++ Ch13_Bitmaps/Ch13_Bitmaps/S6_MadStJames'Park.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ch13_Bitmaps
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class S6_MadStJamesPark : ContentPage
+	{
+		public S6_MadStJamesPark ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
