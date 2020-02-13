5bc26265f0789f3b51ebe6ecf09c9a467f2d0c47
DCoates <DCoates@Rubiconsoftware.com>
2020-02-13 18:30:53 +0000

    Using JsonFlickerFeed

--- Ch13_Bitmaps/Ch13_Bitmaps/S7_ImageBrowser.xaml.cs
+++ Ch13_Bitmaps/Ch13_Bitmaps/S7_ImageBrowser.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,34 +13,85 @@ namespace Ch13_Bitmaps
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class S7_ImageBrowser : ContentPage
 	{
+       
+        readonly WebRequest _request;
+        JsonFlickerFeed _imageList;
+        int _imageListIndex = 0;
+
 		public S7_ImageBrowser ()
 		{
 			InitializeComponent ();
-		}
+
+            Uri uri = new Uri("https://api.flickr.com/services/feeds/photos_public.gne?tags=kitten&format=json&nojsoncallback=1");
+            _request = WebRequest.Create(uri);
+            _request.BeginGetResponse(WebRequestCallback, null);
+        }
+
+        private void WebRequestCallback(IAsyncResult result)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                try
+                {
+                    Stream stream = _request.EndGetResponse(result).GetResponseStream();
+
+                    //Deserialize the JSON into the imageList;
+                    var jsonSerializer = new DataContractJsonSerializer(typeof(JsonFlickerFeed));
+                    _imageList = (JsonFlickerFeed) jsonSerializer.ReadObject(stream ?? throw new InvalidOperationException());
+
+                    if (_imageList.items.Count > 0)
+                        FetchPhoto();
+                }
+                catch (Exception e)
+                {
+                    LFileName.Text = e.Message;
+                }
+            });
+        }
 
         private void OnImagePropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            LFileName.Text = "Ready";
+            if (e.PropertyName == "IsLoading")
+            {
+                ActivityInd1.IsRunning = ((Image) sender).IsLoading;
+            }
         }
 
         private void OnPrevButClicked(object sender, EventArgs e)
         {
-            ActivityInd1.IsEnabled = false;
-            ActivityInd1.IsRunning = false;
-
-            NextButton.IsEnabled = true;
-            PrevButton.IsEnabled = false;
+            _imageListIndex--;
+            FetchPhoto();
         }
 
         private void OnNextButClicked(object sender, EventArgs e)
         {
-            ActivityInd1.IsEnabled = true;
-            ActivityInd1.IsRunning = true;
+            _imageListIndex++;
+            FetchPhoto();
 
-            PrevButton.IsEnabled = true;
-            NextButton.IsEnabled = false;
+        }
 
+        private void FetchPhoto()
+        {
+            // Prepare for new image.
+            Image1.Source = null;
+            string url = _imageList.items[_imageListIndex].link;
 
+            //Set the filename
+            LFileName.Text = url.Substring(url.LastIndexOf('/') + 1);
+
+            // Create the UriImageSource
+            UriImageSource imageSource = new UriImageSource
+            {
+                Uri = new Uri(url + "?Width=1080"),
+                CacheValidity = TimeSpan.FromDays(30)
+            };
+
+            // Set the Image Source.
+            Image1.Source = imageSource;
+
+            // Enable or disable buttons
+            PrevButton.IsEnabled = _imageListIndex > 0;
+            NextButton.IsEnabled = _imageListIndex < _imageList.items.Count - 1;
         }
     }
 }
