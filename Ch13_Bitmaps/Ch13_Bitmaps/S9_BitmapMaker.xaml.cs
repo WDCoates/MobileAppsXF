639e8d17196ce92c97f97c74cd05ad1e6eb92019
DCoates <DCoates@Rubiconsoftware.com>
2020-02-19 09:12:20 +0000

    A finished BitmapMaker Red to Blue fade down the page!

--- Ch13_Bitmaps/Ch13_Bitmaps/S9_BitmapMaker.xaml.cs
+++ Ch13_Bitmaps/Ch13_Bitmaps/S9_BitmapMaker.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Ch13_Bitmaps
@@ -15,6 +9,19 @@ namespace Ch13_Bitmaps
 		public S9_BitmapMaker ()
 		{
 			InitializeComponent ();
-		}
+
+            var rows = 128;
+            var cols = 64;
+
+            BmpMaker bmpMaker = new BmpMaker(cols, rows);
+
+            for (int row = 0; row < rows; row++)
+                for(int col=0; col < cols; col++)
+                {
+                    bmpMaker.SetPixel(row, col, 2 * row, 0, 2 * (128 - row), 0);
+                }
+
+            Image.Source = bmpMaker.Generate();
+        }
 	}
 }
