1821cc3dbaa1a7dd39f6c51bdefb2a3510c1ce3d
DCoates <DCoates@Rubiconsoftware.com>
2020-03-09 09:08:53 +0000

    Chapter 14 Absolute Layout Demo S1

---
+++ Ch14_AbsoluteLayout/Ch14_AbsoluteLayout/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Ch14_AbsoluteLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //S3_ResourceBitmapCode ()
+            ButtonS1.Clicked += (sender, args) => { Navigation.PushAsync(new S1_AbsoluteDemo()); };
+        }
+    }
+}
